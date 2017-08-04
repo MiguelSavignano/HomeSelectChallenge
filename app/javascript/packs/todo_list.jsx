cb4719c875cba83630b1b6d61820ab08f6ebe099
Miguel Savignano <migue.masx@gmail.com>
2017-08-04 08:35:35 +0200

    test concept todo list

--- app/javascript/packs/todo_list.jsx
+++ app/javascript/packs/todo_list.jsx
@@ -2,21 +2,43 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import PropTypes from 'prop-types'
 
-const Hello = props => (
-  <div>Hello {props.name}!</div>
-)
+class TodoListApp extends React.Component {
 
-Hello.defaultProps = {
-  name: 'David'
-}
+  state = { todo: {value: ""}, todos: [] }
 
-Hello.propTypes = {
-  name: PropTypes.string
+  onAddTodo = () => {
+    if (this.state.todo.value == "") return false
+    this.setState({ todos: [...this.state.todos, this.state.todo], todo: {value: ""} })
+  }
+
+  onChangeHandler = (event) => {
+    this.setState({ todo: { value: event.target.value } })
+  }
+
+  getColorItem = (index) => {
+   if ((index + 1) % 3 == 0) return "red-color"
+   return ""
+  }
+
+  render() {
+    const { todos, todo } = this.state
+    return (
+      <div>
+        <input type="text" value={todo.value} onChange={this.onChangeHandler} />
+        <button onClick={this.onAddTodo}>Insert</button>
+        <ul>
+          { todos.map((todo, index) => (
+            <li key={index} className={this.getColorItem(index)}>{todo.value}</li>
+          ))}
+        </ul>
+      </div>
+    )
+  }
 }
 
 document.addEventListener('DOMContentLoaded', () => {
   ReactDOM.render(
-    <Hello name="React" />,
+    <TodoListApp />,
     document.querySelector('#todo-list-app'),
   )
 })
