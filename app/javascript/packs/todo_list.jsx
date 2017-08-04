74a8efb37d32bd36380bc5ebcb713ac17ac46d43
Miguel Savignano <migue.masx@gmail.com>
2017-08-04 09:05:42 +0200

    add functions to save state in the local store

--- app/javascript/packs/todo_list.jsx
+++ app/javascript/packs/todo_list.jsx
@@ -2,26 +2,46 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import PropTypes from 'prop-types'
 
+const genetageUid = () => {
+  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+    var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8)
+    return v.toString(16)
+  });
+}
+
 class TodoListApp extends React.Component {
 
   state = { todo: {value: ""}, todos: [] }
 
-  genetageUid = () => {
-    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
-      var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8)
-      return v.toString(16)
-    });
+  componentDidMount() {
+    this.getTodosFromLocalStore()
+  }
+
+  saveTodos = () => {
+    localStorage.setItem('todos', JSON.stringify(this.state.todos));
+  }
+
+  getTodosFromLocalStore = () => {
+    const todos = JSON.parse(localStorage.getItem('todos') || [])
+    this.setState({ todos })
   }
 
   onAddTodo = () => {
     if (this.state.todo.value == "") return false
     const todo = this.state.todo
-    todo.id = this.genetageUid()
-    this.setState({ todos: [...this.state.todos, todo], todo: {value: ""} })
+    todo.id = genetageUid()
+    this.setState({ todos: [...this.state.todos, todo], todo: {value: ""} }, () => {
+      this.saveTodos()
+    })
   }
 
   removeTodo = todoSelected => event => {
-    this.setState({ todos: this.state.todos.filter(todo => todoSelected.id != todo.id), todo: { value: ""} })
+    this.setState({
+      todos: this.state.todos.filter(todo => todoSelected.id != todo.id),
+      todo: { value: ""}
+    }, () => {
+      this.saveTodos()
+    })
   }
 
   onChangeHandler = (event) => {
