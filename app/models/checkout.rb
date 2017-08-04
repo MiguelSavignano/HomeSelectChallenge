d251833c44755ec234e273cbe64d6302d23fd0ed
Miguel Savignano <migue.masx@gmail.com>
2017-08-04 06:39:31 +0200

    .

--- app/models/checkout.rb
+++ app/models/checkout.rb
@@ -8,4 +8,14 @@ class Checkout < ApplicationRecord
   def scan(product)
     @products << product
   end
+
+  def total
+    total = @products.map(&:price).reduce(&:+)
+    discounts = calculate_discount(@products)
+    total - discounts
+  end
+
+  def calculate_discount(products)
+    0
+  end
 end
